728b075e4ea62e9f52c9ecad51773ea1e55abbc7
fauzigalih <fauzigalihas@gmail.com>
2020-07-01 20:58:25 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SistemPakarFuzzyMamdani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
