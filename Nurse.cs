0371c17f44ed51e0e789babbbd6daf7f723b2636
fauzigalih <fauzigalihas@gmail.com>
2020-07-01 22:43:26 +0700

    add model nurse

---
+++ Nurse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SistemPakarFuzzyMamdani
+{
+    class Nurse
+    {
+        public int NurseNIP { get; set; }
+        public string NurseName { get; set; }
+        public string NurseAge { get; set; }
+        public double ScoreFinal { get; set; }
+        public string Note { get; set; }
+
+        public bool ConfirmNIP(int nip) => nip == 6;
+        public bool ConfirmAge(int age) => age > 17;
+        public bool TestWrite(int x) => x >= 20 && x <= 90;
+        public bool TestPractice(int x) => x >= 20 && x <= 90;
+    }
+}
