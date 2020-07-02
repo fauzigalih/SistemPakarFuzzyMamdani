274e0f5a5da9f710236f550fac477fdc898845d0
fauzigalih <fauzigalihas@gmail.com>
2020-07-02 14:58:51 +0700

    new feature model

--- Nurse.cs
+++ Nurse.cs
@@ -6,13 +6,16 @@ namespace SistemPakarFuzzyMamdani
     {
         public int NurseNIP { get; set; }
         public string NurseName { get; set; }
-        public string NurseAge { get; set; }
+        public int NurseAge { get; set; }
         public double ScoreFinal { get; set; }
         public string Note { get; set; }
 
+        public int TestWrite { get; set; }
+        public int TestPractice { get; set; }
+
         public bool ConfirmNIP(int nip) => nip == 6;
         public bool ConfirmAge(int age) => age > 17;
-        public bool TestWrite(int x) => x >= 20 && x <= 90;
-        public bool TestPractice(int x) => x >= 20 && x <= 90;
+        public bool ConfirmTestWrite(int x) => x >= 20 && x <= 90;
+        public bool ConfirmTestPractice(int x) => x >= 20 && x <= 90;
     }
 }
