65cea8b41fb4ce3c59eca9bee3b30f481226db1f
fauzigalih <fauzigalihas@gmail.com>
2020-07-03 22:53:16 +0700

    update nurse and refactor

--- Nurse.cs
+++ Nurse.cs
@@ -4,18 +4,15 @@ namespace SistemPakarFuzzyMamdani
 {
     class Nurse
     {
+        static Setting set = new Setting();
+
         public int NurseNIP { get; set; }
         public string NurseName { get; set; }
         public int NurseAge { get; set; }
         public double ScoreFinal { get; set; }
         public string Note { get; set; }
 
-        public int TestWrite { get; set; }
-        public int TestPractice { get; set; }
-
-        public bool ConfirmNIP(int nip) => nip == 6;
-        public bool ConfirmAge(int age) => age > 17;
-        public bool ConfirmTestWrite(int x) => x >= 20 && x <= 90;
-        public bool ConfirmTestPractice(int x) => x >= 20 && x <= 90;
+        public bool ConfirmNIP(int nip) => nip == set.nip;
+        public bool ConfirmAge(int age) => age > set.age;
     }
 }
