a7b406743f9ad789549d648aae7286e46db0f5a1
fauzigalih <fauzigalihas@gmail.com>
2020-07-03 22:53:28 +0700

    update program and refactor

--- Program.cs
+++ Program.cs
@@ -9,9 +9,13 @@ namespace SistemPakarFuzzyMamdani
         static bool _error = false;
         static string messageError;
 
+        static Fuzzy fuzzy = new Fuzzy();
         static Score score = new Score();
         static Nurse nurse = new Nurse();
+        static Setting set = new Setting();
+
         static List<Nurse> nurseList = new List<Nurse>();
+
         static void Main()
         {
             ConfirmPassword();
@@ -21,18 +25,17 @@ namespace SistemPakarFuzzyMamdani
         {
             Welcome();
             InputData();
-            Console.WriteLine();
             InputScore();
+            ConfirmProcess();
         }
 
         static void ConfirmPassword()
         {
-            int password = 1234;
             while(!_password)
             {
                 Console.Write("Masukan password utk melanjutkan: ");
                 string answer = Console.ReadLine();
-                if (answer == password.ToString())
+                if (answer == set.password.ToString())
                 {
                     _password = true;
                     Console.Clear();
@@ -46,14 +49,25 @@ namespace SistemPakarFuzzyMamdani
             }
         }
 
+        static void ConfirmProcess()
+        {
+            Console.WriteLine();
+            Console.Write("Apakah data diatas sudah benar dan ingin melanjutkan proses?(y/n) : ");
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "y" || answer == "yes" || answer == "ya") Calculation();
+            else if (answer == "n" || answer == "no") Home();
+            else Console.WriteLine("Tidak ada pilihan yg tersedia.");
+        }
+
         static void Welcome()
         {
             Console.WriteLine("Selamat Datang di Sistem Pakar \n" +
-                "Aplikasi Sertifikasi Perawat \n\n");
+                "Aplikasi Sertifikasi Perawat \n");
         }
 
         static void InputData()
         {
+            Console.WriteLine();
             if (_error) Console.WriteLine(messageError + ", masukan ulang kembali data anda dgn benar!");
 
             Console.Write("Masukan NIP: ");
@@ -64,7 +78,7 @@ namespace SistemPakarFuzzyMamdani
                 Console.Clear();
                 _error = true;
                 messageError = "NIP harus 6 digit";
-                Home();
+                RefreshInputData();
             }
             else
             {
@@ -80,50 +94,130 @@ namespace SistemPakarFuzzyMamdani
                     Console.Clear();
                     _error = true;
                     messageError = "Umur harus diatas 17 thn";
-                    Home();
+                    RefreshInputData();
                 }
                 else _error = false;
             }
         }
 
-        static void _InputData()
+        static void _InputData(bool cache = true)
         {
-            Console.WriteLine("Masukan NIP: " + nurse.NurseNIP);
-            Console.WriteLine("Masukan Nama: " + nurse.NurseName);
-            Console.WriteLine("Masukan Umur: " + nurse.NurseAge);
+            string add = "Masukan ";
+            Console.WriteLine();
+            Console.WriteLine((cache ? add : "") + "NIP: " + nurse.NurseNIP);
+            Console.WriteLine((cache ? add : "") + "Nama: " + nurse.NurseName);
+            Console.WriteLine((cache ? add : "") + "Umur: " + nurse.NurseAge);
         }
 
         static void InputScore()
         {
+            Console.WriteLine();
             if (_error) Console.WriteLine(messageError + ", masukan ulang kembali data anda dgn benar!");
 
-            Console.Write("Masukan Nilai Tes Tulis: ");
-            int testWrite = Convert.ToInt32(Console.ReadLine());
-            nurse.TestWrite = testWrite;
-            if (!nurse.ConfirmTestWrite(testWrite))
+            Console.Write("Masukan Nilai Tes Tulis ({0}-{1}): ", set.writeLow, set.writeHigh);
+            double testWrite = Convert.ToDouble(Console.ReadLine());
+            score.TestWrite = testWrite;
+            if (!score.ConfirmTestWrite(testWrite))
             {
                 Console.Clear();
                 _error = true;
-                messageError = "Salah Tulis";
-                Welcome();
-                _InputData();
-                InputScore();
+                messageError = $"Nilai Tes Tulis harus {set.writeLow}-{set.writeHigh}";
+                RefreshInputScore();
             }
-            else _error = false;
+            else
+            {
+                _error = false;
+                Console.Write("Masukan Nilai Tes Praktek ({0}-{1}): ", set.practiceLow, set.practiceHigh);
+                double testPractice = Convert.ToDouble(Console.ReadLine());
+                score.TestPractice = testPractice;
+                if (!score.ConfirmTestPractice(testPractice))
+                {
+                    Console.Clear();
+                    _error = true;
+                    messageError = $"Nilai Tes Praktek harus {set.practiceLow}-{set.practiceHigh}";
+                    RefreshInputScore();
+                }
+                else _error = false;
+            }
+        }
 
-            Console.Write("Masukan Nilai Tes Praktek: ");
-            int testPractice = Convert.ToInt32(Console.ReadLine());
-            nurse.TestPractice = testPractice;
-            if (!nurse.ConfirmTestPractice(testPractice))
-            {
-                Console.Clear();
-                _error = true;
-                messageError = "Salah praktek";
-                Welcome();
-                _InputData();
-                InputScore();
-            }
-            else _error = false;
+        static void _InputScore(bool cache = true)
+        {
+            string add = "Masukan ";
+            string write = $"({set.writeLow}-{set.writeHigh})";
+            string practice = $"({set.practiceLow}-{set.practiceHigh})";
+            Console.WriteLine();
+            Console.WriteLine((cache ? add : "") + $"Nilai Tes Tulis {(cache ? write : "\u0008")}: {score.TestWrite}");
+            Console.WriteLine((cache ? add : "") + $"Nilai Tes Praktek {(cache ? practice : "\u0008")}: {score.TestPractice}");
+        }
+
+        static void RefreshInputData()
+        {
+            Welcome();
+            InputData();
+        }
+
+        static void RefreshInputScore()
+        {
+            Welcome();
+            _InputData();
+            InputScore();
+        }
+
+        static void Calculation()
+        {
+            Console.Clear();
+            Welcome();
+            _InputData(false);
+            _InputScore(false);
+            Level();
+            Conjunction();
+            Disjunction();
+            Defuzzyfication();
+            ResultTest();
+        }
+
+        static void Level()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Derajat Keanggotaan: \n" +
+                $"Tes Tulis Kurang: {fuzzy.writeLowLevel} \n" +
+                $"Tes Tulis Cukup: {fuzzy.writeMidLevel} \n" +
+                $"Tes Tulis Baik: {fuzzy.writeHighLevel} \n" +
+                $"Tes Praktek Kurang: {fuzzy.practiceLowLevel} \n" +
+                $"Tes Praktek Cukup: {fuzzy.practiceMidLevel} \n" +
+                $"Tes Praktek Baik: {fuzzy.practiceHighLevel}");
+        }
+
+        static void Conjunction()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Konjungsi: \n" +
+                $"Tes Tulis Kurang: {fuzzy.writeLowLevel} AND Tes Praktek Kurang: {fuzzy.practiceLowLevel} \n" +
+                $"Tes Tulis Kurang: {fuzzy.writeLowLevel} AND Tes Praktek Cukup: {fuzzy.practiceMidLevel} \n" +
+                $"Tes Tulis Cukup: {fuzzy.writeMidLevel} AND Tes Praktek Kurang: {fuzzy.practiceLowLevel} \n" +
+                $"Tes Tulis Cukup: {fuzzy.writeMidLevel} AND Tes Praktek Cukup: {fuzzy.practiceMidLevel}");
+        }
+
+        static void Disjunction()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Disjungsi: \n" +
+                $"Tes Tulis Kurang: {fuzzy.disjunction1} OR Tes Praktek Kurang: {fuzzy.disjunction3} THEN Tidak Lulus {fuzzy.graduateNot} \n" +
+                $"Tes Tulis Cukup: {fuzzy.disjunction2} OR Tes Praktek Cukup: {fuzzy.disjunction4} THEN Lulus {fuzzy.graduateYes}");
+        }
+
+        static void Defuzzyfication()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Defuzzyfikasi: \n" +
+                $"Defuzzyfikasi: {fuzzy.defuzzyfication}");
+        }
+
+        static void ResultTest()
+        {
+            Console.WriteLine();
+            Console.WriteLine($"Hasil dari perhitungan adalah {fuzzy.graduateFinal}");
         }
     }
 }
