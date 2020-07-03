36097e3d8eb7292dec4f3a88fc2dce2608a7c29a
fauzigalih <fauzigalihas@gmail.com>
2020-07-03 22:53:41 +0700

    update score and refactor

--- Score.cs
+++ Score.cs
@@ -1,10 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SistemPakarFuzzyMamdani
 {
     class Score
     {
+        static Setting set = new Setting();
+
+        public double TestWrite { get; set; } = 40;
+        public double TestPractice { get; set; } = 40;
+
+        public bool ConfirmTestWrite(double write) => write >= set.writeLow && write <= set.writeHigh;
+        public bool ConfirmTestPractice(double practice) => practice >= set.practiceLow && practice <= set.practiceHigh;
     }
 }
